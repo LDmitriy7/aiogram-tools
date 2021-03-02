05ccc0d2f74c7fb0611a62e2b5e683d3e7b9cb91
Dmitriy <levchenko.d.a1998@gmail.com>
2021-03-03 00:55:31 +0300

    .

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
 setup(
-    packages=['aiogram_tools'],
-    python_requires='>=3.9',
+    # packages=['aiogram_tools'],
+    # python_requires='>=3.9',
     setup_requires=['pbr'], pbr=True,
-
     install_requires=[
         'aiogram>=2.11.2',
     ],
